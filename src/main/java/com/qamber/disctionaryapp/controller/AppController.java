5a26e5ce142c17acee673b57a8c8b1a6c5dcb9f7
Qamberhasnain <qamber.qh786@gmail.com>
2022-05-07 16:01:18 +0530

    Fist

--- src/main/java/com/qamber/disctionaryapp/controller/AppController.java
+++ src/main/java/com/qamber/disctionaryapp/controller/AppController.java
@@ -3,6 +3,7 @@ package com.qamber.disctionaryapp.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,18 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Controller
 @RequestMapping("/meaning")
 public class AppController {
 	
 	
 	@RequestMapping()
-	public List<Object> getMeaning(@RequestParam String word, Model model){
+	public String getMeaning(@RequestParam String word, Model model){
 			String url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 			RestTemplate restTemplate = new RestTemplate();
 			Object[] data = restTemplate.getForObject(url+word,Object[].class);
-			
-			return Arrays.asList(data);
+			model.addAttribute("message",Arrays.asList(data));
+			return "success";
 			//return is not working... so I will watch session19 to understand the redirection pages and put response.
 	}
 	
